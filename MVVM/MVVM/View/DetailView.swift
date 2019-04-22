2b7c630f7681d0dfbdff41fe0d76bb954cbe5056
ViniciusDeep <viniciuscorreia11@gmail.com>
2019-04-22 07:12:42 -0300

    Add MVVM and copy files

---
+++ MVVM/MVVM/View/DetailView.swift
@@ -0,0 +1,76 @@
+//
+//  DetailView.swift
+//  MVC
+//
+//  Created by Vinicius Mangueira Correia on 21/04/19.
+//  Copyright © 2019 Vinicius Mangueira Correia. All rights reserved.
+//
+
+import UIKit
+
+
+
+class DetailView: UIView {
+   lazy var artistName: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    lazy var trackName: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    lazy var primaryGenreName: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    
+    lazy var trackPrice: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    lazy var imageTrack: UIImageView = {
+        let image = UIImageView(image: UIImage(named: "search"))
+        image.translatesAutoresizingMaskIntoConstraints = false
+        image.frame = CGRect(x: 0, y: 0, width: 200, height: 200)
+        return image
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupSubViews()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    fileprivate func setupSubViews() {
+        addSubview(artistName)
+        addSubview(trackName)
+        addSubview(trackPrice)
+        addSubview(primaryGenreName)
+        addSubview(imageTrack)
+        setConstraints()
+    }
+    
+    fileprivate func setConstraints() {
+        artistName.fillSuperview(padding: .init(top: 16, left: 16, bottom: 16, right: 16))
+        
+        
+        imageTrack.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
+        imageTrack.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
+        
+        
+    }
+}
+
+
+
---
+++ MVVM/MVVM/View/ListCell.swift
@@ -0,0 +1,60 @@
+//
+//  ListCell.swift
+//  MVC
+//
+//  Created by Vinicius Mangueira Correia on 21/04/19.
+//  Copyright © 2019 Vinicius Mangueira Correia. All rights reserved.
+//
+
+import UIKit
+import SDWebImage
+
+class ListCell: UITableViewCell {
+    
+    var result: Result! {
+        didSet {
+            nameLabel.text = result.trackName
+            imageTrack.sd_setImage(with: URL(string: result.artworkUrl100))
+        }
+    }
+    
+    lazy var nameLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    lazy var imageTrack: UIImageView = {
+        let iv = UIImageView()
+        iv.translatesAutoresizingMaskIntoConstraints = false
+        iv.widthAnchor.constraint(equalToConstant: 40).isActive = true
+        iv.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        iv.layer.cornerRadius = 12
+        iv.clipsToBounds = true
+        return iv
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setUpComponentsInCell()
+    }
+    
+    
+    fileprivate func setUpComponentsInCell() {
+        addSubview(nameLabel)
+        addSubview(imageTrack)
+        setConstraintsInLabel()
+    }
+    
+    fileprivate func setConstraintsInLabel() {
+        imageTrack.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 10).isActive = true
+        nameLabel.leadingAnchor.constraint(equalToSystemSpacingAfter: imageTrack.leadingAnchor, multiplier: 7).isActive = true
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+}
